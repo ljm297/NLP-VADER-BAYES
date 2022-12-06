9ec29a1fe056200dffeccc669b16a56b51cf605e
ljm297 <jl10321@nyu.edu>
2022-12-05 19:57:30 -0500

    category = 1 tempdata

--- category_filter.py
+++ category_filter.py
@@ -13,3 +13,21 @@ print(res.info())
 
 f = open("./input/youtube/temp_videos.csv",'w')
 res.to_csv("./input/youtube/temp_videos.csv", index=False)
+
+main = pd.read_csv("./input/UScomments.csv",error_bad_lines=False)
+par = pd.read_csv("./input/youtube/temp_videos.csv",error_bad_lines=False)
+
+#transform the dataframes to lists of dictionaries
+main_dict=main.to_dict(orient='records')
+par_dict=par.to_dict(orient='records')
+
+#create a list of dictionaries that use only 'label' and 'value' as keys
+par_dict = [{'video_id':i['video_id']} for i in par_dict]
+
+#search for records in main that the pair of label-value exists in the previous list
+result = [i for i in main_dict if {'video_id':i['video_id']} in par_dict]
+
+#change back to dataframe and save to csv
+result=pd.DataFrame(result)
+
+result.to_csv('./input/youtube/temp_comments.csv', index=False)
