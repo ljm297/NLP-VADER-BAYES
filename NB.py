1cb3687f1cadf927f4f77c5bae4595a899d10954
ljm297 <jl10321@nyu.edu>
2022-12-11 11:51:43 -0500

    bayes w/o undersample

--- NB.py
+++ NB.py
@@ -1,18 +1,12 @@
-import re
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score
 import math
 import nltk
-from sklearn.feature_extraction.text import CountVectorizer
-from collections import defaultdict
-
-# data = pd.read_csv('IMDB Dataset.csv')
-
-
+from nltk.tokenize import tokenize
+from nltk.tokenize import word_tokenize,WhitespaceTokenizer
+def laplace_smoothing(n_label_items, vocab, word_counts, word, text_label):
+    a = word_counts[text_label][word] + 1
+    b = n_label_items[text_label] + len(vocab)
+    return math.log(a/b)
 
 def group_by_label(x, y, labels):
     data = {}
@@ -28,12 +22,14 @@ def fit(x, y, labels):
     for l, data in grouped_data.items():
         n_label_items[l] = len(data)
         log_label_priors[l] = math.log(n_label_items[l] / n)
-     return n_label_items, log_label_priors
+    return n_label_items, log_label_priors
 
 def predict(n_label_items, vocab, word_counts, log_label_priors, labels, x):
     result = []
     for text in x:
+        w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
         label_scores = {l: log_label_priors[l] for l in labels}
+        print("text:",text)
         words = set(w_tokenizer.tokenize(text))
         for word in words:
             if word not in vocab: continue
